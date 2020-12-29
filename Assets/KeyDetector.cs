cac979b60d557078d2e9f4e6bd415261d77db6f8
SamT <samueltang@MacBook-Pro.local>
2020-12-28 22:59:11 -0500

    init code

---
+++ Assets/KeyDetector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class KeyDetector : MonoBehaviour
+{
+    private TextMeshPro playerTextOutput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTextOutput = GameObject.FindGameObjectWithTag("PlayerTextOutput").GetComponentInChildren<TextMeshPro>();
+    }
+
+    private void OnTriggerEnter(Collider other){
+        var key = other.GetComponentInChildren<TextMeshPro>();
+        if(key != null){
+            if(other.gameObject.GetComponent<KeyFeedback>().keyCanBeHitAgain){
+                if(key.text == "SPACE"){
+                    playerTextOutput.text += " ";
+                }else if(key.text == "<-"){
+                    playerTextOutput.text = playerTextOutput.text.Substring(0, playerTextOutput.text.Length-1);
+                }else{
+                    playerTextOutput.text += key.text;
+                }
+            }
+        }
+        
+    }
+}
---
+++ Assets/KeyPressed.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPressed : MonoBehaviour
+{
+    public bool keyHit = false;
+    public bool keyCanBeHitAgain = false;
+    private float originalYPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalYPosition = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(keyHit){
+            keyCanBeHitAgain = false;
+            keyHit = false;
+            transform.position += new Vector3(0, -0.03f, 0);
+        }   
+        if(transform.position.y < originalYPosition){
+            transform.position += new Vector3(0, 0.005f, 0);
+        } else{
+            keyCanBeHitAgain = true;
+        }
+    }
+    
+    void PressButton(){
+        keyHit = true;
+    }
+}
